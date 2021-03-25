a41a4a307899c5066835666197ba7eba74e641da
SoraTrails <me876657621@126.com>
2021-03-25 17:40:37 +0800

    WIP flexpret_io改回inline

--- src/os/include/flexpret_io.h
+++ src/os/include/flexpret_io.h
@@ -7,36 +7,36 @@
 // GPO ports, if port width < 32, then upper bits ignored
 // CSR_GPO_*
 // Write all GPO bits
-inline void gpo_write_0(uint32_t val);
-inline void gpo_write_1(uint32_t val);
-inline void gpo_write_2(uint32_t val);
-inline void gpo_write_3(uint32_t val);
+static inline void gpo_write_0(uint32_t val) { write_csr(uarch4, val); }
+static inline void gpo_write_1(uint32_t val) { write_csr(uarch5, val); }
+static inline void gpo_write_2(uint32_t val) { write_csr(uarch6, val); }
+static inline void gpo_write_3(uint32_t val) { write_csr(uarch7, val); }
 
 // For each '1' bit in mask, set corresponding GPO bit to '1'
-inline void gpo_set_0(uint32_t mask);
-inline void gpo_set_1(uint32_t mask);
-inline void gpo_set_2(uint32_t mask);
-inline void gpo_set_3(uint32_t mask);
+static inline void gpo_set_0(uint32_t mask) { set_csr(uarch4, mask); }
+static inline void gpo_set_1(uint32_t mask) { set_csr(uarch5, mask); }
+static inline void gpo_set_2(uint32_t mask) { set_csr(uarch6, mask); }
+static inline void gpo_set_3(uint32_t mask) { set_csr(uarch7, mask); }
 
 // For each '1' bit in mask, set corresponding GPO bit to '0'
-inline void gpo_clear_0(uint32_t mask);
-inline void gpo_clear_1(uint32_t mask);
-inline void gpo_clear_2(uint32_t mask);
-inline void gpo_clear_3(uint32_t mask);
+static inline void gpo_clear_0(uint32_t mask) { clear_csr(uarch4, mask); }
+static inline void gpo_clear_1(uint32_t mask) { clear_csr(uarch5, mask); }
+static inline void gpo_clear_2(uint32_t mask) { clear_csr(uarch6, mask); }
+static inline void gpo_clear_3(uint32_t mask) { clear_csr(uarch7, mask); }
 
 // Read GPO bits
-inline uint32_t gpo_read_0();
-inline uint32_t gpo_read_1();
-inline uint32_t gpo_read_2();
-inline uint32_t gpo_read_3();
+static inline uint32_t gpo_read_0() { return read_csr(uarch4); }
+static inline uint32_t gpo_read_1() { return read_csr(uarch5); }
+static inline uint32_t gpo_read_2() { return read_csr(uarch6); }
+static inline uint32_t gpo_read_3() { return read_csr(uarch7); }
 
 // GPI ports, if port width < 32, then upper bits are zero
 // Read GPI bits
 // CSR_GPI_*
-inline uint32_t gpi_read_0();
-inline uint32_t gpi_read_1();
-inline uint32_t gpi_read_2();
-inline uint32_t gpi_read_3();
+static inline uint32_t gpi_read_0() { return read_csr(uarch0); }
+static inline uint32_t gpi_read_1() { return read_csr(uarch1); }
+static inline uint32_t gpi_read_2() { return read_csr(uarch2); }
+static inline uint32_t gpi_read_3() { return read_csr(uarch3); }
 
 // TODO: move to flexpret_debug?
 // Some should be in .c file...
@@ -58,10 +58,10 @@ char* itoa_hex_removing_ldz(uint32_t n);
 void emulator_outputstr(const char* str);
 
 // TEMP location
-inline uint32_t get_cycle();
-inline uint32_t get_cycleh();
-inline uint32_t get_instret();
-inline uint32_t get_instreth();
+static inline uint32_t get_cycle() { return read_csr(cycle); }
+static inline uint32_t get_cycleh() { return read_csr(cycleh); }
+static inline uint32_t get_instret() { return read_csr(instret); }
+static inline uint32_t get_instreth() { return read_csr(instreth); }
 
 struct stats{
     uint32_t cycle;
@@ -70,8 +70,21 @@ struct stats{
     uint32_t instreth;
 };
 
-inline void stats_get(struct stats* s);
+static inline void stats_get(struct stats* s) {
+    do {
+        s->cycleh = get_cycleh();
+        s->instreth = get_instreth();
+        s->cycle = get_cycle();
+        s->instret = get_instret();
+    } while((get_cycleh() != s->cycleh) || (get_instreth() != s->instreth));
+}
 
-inline void stats_print(struct stats* s, struct stats* e);
-
+static inline void stats_print(struct stats* s, struct stats* e) {
+    debug_string(itoa_hex(e->cycleh - s->cycleh));
+    debug_string(itoa_hex(e->cycle - s->cycle));
+    debug_string("\n");
+    debug_string(itoa_hex(e->instreth - s->instreth));
+    debug_string(itoa_hex(e->instret - s->instret));
+    debug_string("\n");
+}
 #endif
--- src/os/src/flexpret_io.c
+++ src/os/src/flexpret_io.c
@@ -1,53 +1,7 @@
 #include "flexpret_io.h"
 #include "flexpret_threads.h"
 
-void gpo_write_0(uint32_t val) { write_csr(uarch4, val); }
-void gpo_write_1(uint32_t val) { write_csr(uarch5, val); }
-void gpo_write_2(uint32_t val) { write_csr(uarch6, val); }
-void gpo_write_3(uint32_t val) { write_csr(uarch7, val); }
-
-void gpo_set_0(uint32_t mask) { set_csr(uarch4, mask); }
-void gpo_set_1(uint32_t mask) { set_csr(uarch5, mask); }
-void gpo_set_2(uint32_t mask) { set_csr(uarch6, mask); }
-void gpo_set_3(uint32_t mask) { set_csr(uarch7, mask); }
-
-void gpo_clear_0(uint32_t mask) { clear_csr(uarch4, mask); }
-void gpo_clear_1(uint32_t mask) { clear_csr(uarch5, mask); }
-void gpo_clear_2(uint32_t mask) { clear_csr(uarch6, mask); }
-void gpo_clear_3(uint32_t mask) { clear_csr(uarch7, mask); }
-
-uint32_t gpo_read_0() { return read_csr(uarch4); }
-uint32_t gpo_read_1() { return read_csr(uarch5); }
-uint32_t gpo_read_2() { return read_csr(uarch6); }
-uint32_t gpo_read_3() { return read_csr(uarch7); }
-
-uint32_t gpi_read_0() { return read_csr(uarch0); }
-uint32_t gpi_read_1() { return read_csr(uarch1); }
-uint32_t gpi_read_2() { return read_csr(uarch2); }
-uint32_t gpi_read_3() { return read_csr(uarch3); }
-
-uint32_t get_cycle() { return read_csr(cycle); }
-uint32_t get_cycleh() { return read_csr(cycleh); }
-uint32_t get_instret() { return read_csr(instret); }
-uint32_t get_instreth() { return read_csr(instreth); }
-
-void stats_get(struct stats* s) {
-    do {
-        s->cycleh = get_cycleh();
-        s->instreth = get_instreth();
-        s->cycle = get_cycle();
-        s->instret = get_instret();
-    } while((get_cycleh() != s->cycleh) || (get_instreth() != s->instreth));
-}
-
-void stats_print(struct stats* s, struct stats* e) {
-    debug_string(itoa_hex(e->cycleh - s->cycleh));
-    debug_string(itoa_hex(e->cycle - s->cycle));
-    debug_string("\n");
-    debug_string(itoa_hex(e->instreth - s->instreth));
-    debug_string(itoa_hex(e->instret - s->instret));
-    debug_string("\n");
-}
+static char qbuf[FLEXPRET_HW_THREADS_NUMS][9];
 
 void emulator_outputstr(const char* str) {
     volatile char* addr = (char*) (EMULATOR_ADDR);
@@ -56,8 +10,6 @@ void emulator_outputstr(const char* str) {
         str++;
     }
 }
-
-char qbuf[FLEXPRET_HW_THREADS_NUMS][9];
 
 // TODO : itoa_hex & itoa_hex_removing_ldz is not thread safe
 char* itoa_hex(uint32_t n)
